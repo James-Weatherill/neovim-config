0918f4c3d3ce9f3b930a17b8581c181fb255ad17
James Weatherill <weatherill.james@icloud.com>
2023-11-08 10:02:39 +0000

    full?

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+-- This line is making the init.lua, link to vim-plug --
+
+vim.cmd [[source /Users/james/.config/nvim/vim-plug/plugins.vim]]
+
+
+-- Requirements --
+
+require('coc')
+require('colorschemes')
+require('keymappings')
+require('treesitter')
+require('settings')
+
+-- --
